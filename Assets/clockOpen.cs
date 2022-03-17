cf47e1340d79f966c74b35ca5d24307b1ccaf1d0
LPares01 <95408250+LPares01@users.noreply.github.com>
2022-03-17 11:14:33 +0100

    horloge bidouillle

---
+++ Assets/clockOpen.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class clockOpen : MonoBehaviour
+{
+
+    public GameObject aiguille1;
+    public GameObject aiguille2;
+
+    void Update()
+    {
+        Debug.Log(aiguille1.transform.rotation.z);
+        if (-125/180 < aiguille1.transform.rotation.z && aiguille1.transform.rotation.z < -115/180)
+        {
+            if (-5/180 < aiguille2.transform.rotation.z && aiguille2.transform.rotation.z < 5/180)
+            {
+                //ouverture de la cloche
+                Debug.Log("ouvert");
+            }
+        }
+    }
+}
