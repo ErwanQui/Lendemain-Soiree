cd1183abc1b1b867d8baa8f616b6b78eccd67949
Anne-Sophie1497 <95407517+Anne-Sophie1497@users.noreply.github.com>
2022-03-17 12:07:07 +0100

    drawer and key

---
+++ Assets/Scripts/DrawerBehaviour.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class DrawerBehaviour : MonoBehaviour
+{
+    [Tooltip("The key to unlock the chest.")]
+    public GameObject key;
+
+    protected Transform Drawer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Drawer = transform.parent.Find("drawer");
+        Drawer.GetComponent<Interactable>().enabled = false;
+        Drawer.GetComponent<LinearDrive>().enabled = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == key)
+        {
+            Drawer.GetComponent<Interactable>().enabled = true;
+            Drawer.GetComponent<LinearDrive>().enabled = true;
+            key.GetComponent<Interactable>().attachedToHand.DetachObject(key);
+            key.GetComponent<Interactable>().enabled = false;
+            key.GetComponent<Throwable>().enabled = false;
+            key.GetComponent<Collider>().enabled = false;
+            key.GetComponent<Rigidbody>().isKinematic = true;
+            key.transform.parent.eulerAngles = new Vector3(0, 180, 90);
+            key.transform.position = transform.position + transform.right / 10;
+            key.transform.parent.SetParent(transform, true);
+            GetComponent<AudioSource>().Play();
+        }
+    }
+}
